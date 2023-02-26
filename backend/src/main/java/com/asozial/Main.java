20df18d288e14645e8e34b4ba2ae8c67e3b440ec
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-26 16:18:35 +0100

    rewrote run method, added Startup class

--- backend/src/main/java/com/asozial/Main.java
+++ backend/src/main/java/com/asozial/Main.java
@@ -1,12 +1,23 @@
 package com.asozial;
 
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.runtime.Quarkus;
 
 @QuarkusMain
 public class Main {
-    public static void main(String ... args) {
-        System.out.println("Running main method");
-        Quarkus.run(args);
+
+    public static void main(String... args) {
+        Quarkus.run(Startup.class, args);
+    }
+
+    public static class Startup implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            Quarkus.waitForExit();
+            return 0;
+        }
+
     }
 }
