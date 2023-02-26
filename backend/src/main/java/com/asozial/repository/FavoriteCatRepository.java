248a10481327a4d00dd9048e1530a535afc85916
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-26 20:04:19 +0100

    added missing repo scopes

--- backend/src/main/java/com/asozial/repository/FavoriteCatRepository.java
+++ backend/src/main/java/com/asozial/repository/FavoriteCatRepository.java
@@ -3,5 +3,8 @@ package com.asozial.repository;
 import com.asozial.model.FavoriteCat;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class FavoriteCatRepository implements PanacheMongoRepository<FavoriteCat> {
 }
--- backend/src/main/java/com/asozial/repository/PostRepository.java
+++ backend/src/main/java/com/asozial/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.asozial.repository;
 import com.asozial.model.Post;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class PostRepository implements PanacheMongoRepository<Post> {
 }
