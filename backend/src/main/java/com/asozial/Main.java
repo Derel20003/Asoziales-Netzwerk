3d765891ad467130d1185b297006f6b9465a0d26
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-26 16:20:38 +0100

    start updating mongodb on startup

--- backend/src/main/java/com/asozial/Main.java
+++ backend/src/main/java/com/asozial/Main.java
@@ -1,8 +1,15 @@
 package com.asozial;
 
+import com.asozial.model.User;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.runtime.Quarkus;
+
+import javax.inject.Inject;
+import java.util.LinkedList;
+import java.util.List;
 
 @QuarkusMain
 public class Main {
@@ -18,8 +25,28 @@ public class Main {
 
         @Override
         public int run(String... args) throws Exception {
+            // DROP ALL COLLECTIONS
+            getDatabase().getCollection("user").drop();
+
+            // ADD DATA TO COLLECTIONS
+            getDatabase().getCollection("user", User.class).insertMany(createUsers());
+
             Quarkus.waitForExit();
             return 0;
+        }
+
+        public List<User> createUsers() {
+            List<User> users = new LinkedList<>();
+
+            User user = new User();
+            user.name = "dominik";
+            user.email = "d.auinger@yelp.com";
+            user.bio = "i live in hell";
+            user.password = "12345";
+
+            users.add(user);
+
+            return users;
         }
 
         public MongoDatabase getDatabase() {
