184795cd465d355535f115f8ac864067f9939bf4
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-26 15:36:18 +0100

    added PostRepository

---
+++ backend/src/main/java/com/asozial/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.asozial.repository;
+
+import com.asozial.model.Post;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+
+public class PostRepository implements PanacheMongoRepository<Post> {
+}
