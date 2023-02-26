84ca335b8b6f3ccb1571e9331b09414e96f97126
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-26 15:36:55 +0100

    added FavoriteCatRepository

---
+++ backend/src/main/java/com/asozial/repository/FavoriteCatRepository.java
@@ -0,0 +1,7 @@
+package com.asozial.repository;
+
+import com.asozial.model.FavoriteCat;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+
+public class FavoriteCatRepository implements PanacheMongoRepository<FavoriteCat> {
+}
