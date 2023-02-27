6d0d36051b92475a84ff5978aa2d733e2d226b35
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-27 22:49:13 +0100

    updated routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  { path: 'home/', component: HomeComponent },
-  { path: '**', redirectTo: 'home/' }
+  { path: 'home', component: HomeComponent },
+  { path: 'cats', component: HomeComponent },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
