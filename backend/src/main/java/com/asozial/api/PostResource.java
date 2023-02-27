0b63888bed8b7f1fa367242529ab566af692e08f
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-27 11:20:35 +0100

    updated PostResource

--- backend/src/main/java/com/asozial/api/PostResource.java
+++ backend/src/main/java/com/asozial/api/PostResource.java
@@ -3,12 +3,15 @@ package com.asozial.api;
 import com.asozial.model.Post;
 import com.asozial.model.dto.PostDTO;
 import com.asozial.repository.PostRepository;
+import org.bson.types.ObjectId;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -19,17 +22,41 @@ public class PostResource {
     PostRepository postRepository;
 
     @GET
+    @Path("{id}")
+    public Post get(@PathParam("id") String id) {
+        return postRepository.findById(new ObjectId(id));
+    }
+
+    @GET
     @Path("/all")
     public Response getAll() {
         return Response.ok(this.postRepository.listAll()).build();
     }
 
+    @GET
+    @Path("/find")
+    public List<Post> find(@QueryParam("content") String content) {
+        return postRepository.list("content", content);
+    }
+
     @POST
-    public Response add(PostDTO post) {
-        Post newPost = new Post();
-        newPost.content = post.content;
-        newPost.timestamp = LocalDateTime.now();
-        this.postRepository.persist(newPost);
-        return Response.ok().build();
+    public Response add(PostDTO p) {
+        Post post = new Post();
+        post.content = p.content;
+        post.timestamp = LocalDateTime.now();
+        postRepository.persist(post);
+        return Response.created(URI.create(post.id.toString())).build();
+    }
+
+    @PUT
+    public void update(Post post) {
+        postRepository.update(post);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") String id) {
+        postRepository.findByIdOptional(new ObjectId(id))
+                .ifPresent(post -> postRepository.delete(post));
     }
 }
