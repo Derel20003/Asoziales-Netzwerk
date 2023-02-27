a2063f7c2488e0595d0982d23fe09be315c04280
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-27 10:56:14 +0100

    updated UserResource

--- backend/src/main/java/com/asozial/api/UserResource.java
+++ backend/src/main/java/com/asozial/api/UserResource.java
@@ -2,11 +2,14 @@ package com.asozial.api;
 
 import com.asozial.model.User;
 import com.asozial.repository.UserRepository;
+import org.bson.types.ObjectId;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -17,14 +20,38 @@ public class UserResource {
     UserRepository userRepository;
 
     @GET
+    @Path("{id}")
+    public User get(@PathParam("id") String id) {
+        return userRepository.findById(new ObjectId(id));
+    }
+
+    @GET
     @Path("/all")
-    public Response getAll() {
-        return Response.ok(userRepository.listAll()).build();
+    public List<User> getAll() {
+        return userRepository.listAll();
+    }
+
+    @GET
+    @Path("/find")
+    public List<User> find(@QueryParam("name") String name) {
+        return userRepository.list("name", name);
     }
 
     @POST
     public Response add(User user) {
-        this.userRepository.persist(user);
-        return Response.ok().build();
+        userRepository.persist(user);
+        return Response.created(URI.create("/user/" + user.id.toString())).build();
+    }
+
+    @PUT
+    public void update(User user) {
+        userRepository.update(user);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") String id) {
+        userRepository.findByIdOptional(new ObjectId(id))
+                .ifPresent(user -> userRepository.delete(user));
     }
 }
