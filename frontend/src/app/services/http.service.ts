f7d6acb30515f602ca51b72cbda11d5b9002e130
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2023-02-28 09:46:13 +0100

    updated http service, added getAllPosts method

--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  BASE_URL = 'http://localhost:8080/'
+
+  constructor(private http: HttpClient) { }
+
+  getAllPosts(): Observable<any> {
+    return this.http.get<any>(this.BASE_URL + 'post/all')
+  }
 }
